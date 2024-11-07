7713d0cd968471dca346190be58aece764ae4d82
Boris Novák <noboris.gymnazium@gmail.com>
2024-11-07 17:56:46 +0100

    Initial commit

---
+++ graphql-playground/src/main/java/com/bnovak/graphql_playground/GraphqlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.bnovak.graphql_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPlaygroundApplication.class, args);
+	}
+
+}
---
+++ graphql-playground/src/test/java/com/bnovak/graphql_playground/GraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnovak.graphql_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
