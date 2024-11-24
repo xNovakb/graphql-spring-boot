d404bf459bbe057b739c9e92e3b10fdffab88c90
Boris Novák <noboris.gymnazium@gmail.com>
2024-11-24 13:22:23 +0100

    Basic CRUD operation tests with example of Assetions also

--- graphql-playground/src/test/java/com/bnovak/graphql_playground/GraphqlCrudTest.java
+++ graphql-playground/src/test/java/com/bnovak/graphql_playground/GraphqlCrudTest.java
@@ -1,12 +1,17 @@
 package com.bnovak.graphql_playground;
 
+import com.bnovak.graphql_playground.lec16.dto.CustomerDto;
+import com.bnovak.graphql_playground.lec16.dto.DeleteResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.Map;
 
 @SpringBootTest
 @AutoConfigureHttpGraphQlTester
@@ -26,9 +31,60 @@ public class GraphqlCrudTest {
 			}
 			""";
 		this.client.document(doc)
+			.execute()
+			.path("customers").entityList(Object.class).hasSizeGreaterThan(2)
+			.path("customers.[0].name").entity(String.class).isEqualTo("Alice");
+	}
+
+	@Test
+	public void customerByIdTest() {
+		this.client.documentName("crud-operations")
+			.variable("id", 1)
+			.operationName("GetCustomerById")
+			.execute()
+			.path("response.id").entity(Integer.class).isEqualTo(1)
+			.path("response.name").entity(String.class).isEqualTo("Alice")
+			.path("response.age").entity(Integer.class).isEqualTo(25)
+			.path("response.city").entity(String.class).isEqualTo("New York");
+	}
+
+	@Test
+	public void createCustomerTest() {
+		this.client.documentName("crud-operations")
+				.variable("customer", new CustomerDto().setName("Bob").setAge(30).setCity("Los Angeles"))
+				.operationName("CreateCustomer")
 				.execute()
-				.path("customers").entityList(Object.class).hasSizeGreaterThan(2)
-				.path("customers.[0].name").entity(String.class).isEqualTo("Alice");
+				.path("response.id").entity(Integer.class).isEqualTo(11)
+				.path("response.name").entity(String.class).isEqualTo("Bob")
+				.path("response.age").entity(Integer.class).isEqualTo(30)
+				.path("response.city").entity(String.class).isEqualTo("Los Angeles");
+	}
+
+	@Test
+	public void updateCustomerTest() {
+		this.client.documentName("crud-operations")
+				.variables(Map.of("id", 1, "customer", new CustomerDto().setName("Alice").setAge(35).setCity("New York")))
+				.operationName("UpdateCustomer")
+				.execute()
+				.path("response.id").entity(Integer.class).isEqualTo(1)
+				.path("response.name").entity(String.class).isEqualTo("Alice")
+				.path("response.age").entity(Integer.class).isEqualTo(35)
+				.path("response.city").entity(String.class).isEqualTo("New York")
+				.path("response").entity(Object.class).satisfies(System.out::println);
+	}
+
+	@Test
+	public void deleteCustomerTest() {
+		this.client.documentName("crud-operations")
+				.variable("id", 10)
+				.operationName("DeleteCustomer")
+				.execute()
+				.path("response.id").entity(Integer.class).isEqualTo(10)
+				.path("response.success").entity(Boolean.class).isEqualTo(true)
+				.path("response").entity(DeleteResponseDto.class).satisfies(r -> {
+					Assertions.assertThat(r.getId()).isEqualTo(10);
+					Assertions.assertThat(r.getSuccess()).isTrue();
+				});
 	}
 
 }
